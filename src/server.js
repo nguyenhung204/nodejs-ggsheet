d48c723862d9f3f7d726cf9ff8b8aa4f42ee376c
nguyenhung <nguyenhung2004200@gmial.com>
2024-08-26 13:08:31 +0700

    create file when start

--- src/server.js
+++ src/server.js
@@ -3,13 +3,25 @@ import express from "express";
 import bodyParser from "body-parser";
 import initWebRoutes from "./routes/web.js";
 import configViewEngine from "./config/viewEngine.js";
+import { writeDataToCSV } from './config/writeToCSV.js';
+import fs from 'fs';
+
+const filePath = 'public/data.csv';
+const fileExists = fs.existsSync(filePath);
+
+if (!fileExists) {
+    writeDataToCSV([]).then(() => {
+        console.log('File data.csv đã được tạo với tiêu đề.');
+    }).catch(err => {
+        console.error('Lỗi khi tạo file data.csv:', err);
+    });
+}
 
 let app = express();
 
 //use body-parser to post data
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
 
 // Serve static files from the "public" directory
 app.use(express.static('public'));
@@ -20,12 +32,12 @@ configViewEngine(app);
 // init all web routes
 initWebRoutes(app);
 
-
 let port = process.env.PORT || 8080;
 
 app.listen(port, ()=>{
    console.log(`App is running at the port ${port}`);
 });
+
 // Xử lý các lỗi không được bắt
 process.on('unhandledRejection', (reason, promise) => {
    console.error('Unhandled Rejection at:', promise, 'reason:', reason);
